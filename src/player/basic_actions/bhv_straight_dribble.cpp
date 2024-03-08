06916e056cf985675585bbf1699006c4e819fce5
kyo44 <hatakyo3+ser.it@gmail.com>
2024-03-08 17:49:13 +0900

    kickableエリアの任意の割合の正面にボールを置くドリブルが出来るように修正

--- src/player/basic_actions/bhv_straight_dribble.cpp
+++ src/player/basic_actions/bhv_straight_dribble.cpp
@@ -25,11 +25,11 @@ using namespace rcsc;
 bool
 Bhv_StraighDribble::execute( PlayerAgent * agent )
 {
-    const ServerParam & SP = ServerParam::i();
     const WorldModel & wm = agent->world();
+    const ServerParam & SP = ServerParam::i();
 
-    constexpr int dash_cycle = 2;
-    // constexpr double kickable_alea_rate = 0.5;
+    constexpr int dash_cycle = 3;
+    constexpr double kickable_area_rate = 1.0;
 
     dlog.addText( Logger::ACTION,
                   __FILE__, ": Bhv_StraightDribble" );
@@ -40,57 +40,26 @@ Bhv_StraighDribble::execute( PlayerAgent * agent )
     dlog.addText( Logger::TEAM,
                   __FILE__": cycle: %d", s_cycle_after_kick );
 
-    if ( s_cycle_after_kick >= 1 + dash_cycle )
+    if ( s_cycle_after_kick >= 1 + dash_cycle && wm.self().isKickable() )
     {
-        const Vector2D player_move_pos = get_player_move_dist( agent, dash_cycle );
+        const Vector2D after_player_pos = get_player_move_pos( agent, dash_cycle );
 
-        const Vector2D player_to_ball_margin = Vector2D::polar2vector( wm.self().playerType().playerSize() + SP.ballSize() + 0.1,
-                                                                       ( player_move_pos - wm.self().pos() ).dir() );
-        const Vector2D ball_pos = player_move_pos + player_to_ball_margin;
-        dlog.addCircle( Logger::TEAM, ball_pos, 0.05, "black" );
-        dlog.addText( Logger::TEAM,
-                      __FILE__": ball margin: (%.2f, %.2f), (x, y) = (%.2f, %.2f) dir=%.2f",
-                      player_to_ball_margin.x, player_to_ball_margin.y,
-                      ball_pos.x, ball_pos.y, ( player_move_pos - wm.self().pos() ).dir() );
+        const Vector2D target_ball_pos = get_player_to_ball_margin( agent, after_player_pos, kickable_area_rate );
 
         const Vector2D their_goal_pos( SP.pitchLength() / 2.0, 0.0 );
         AngleDeg player_to_goal_angle = ( their_goal_pos - wm.ball().pos() ).dir() - wm.self().body();
 
-        const double first_ball_speed =  SP.firstBallSpeed( ball_pos.dist( wm.ball().pos() ), dash_cycle + 1 );
+        const double first_ball_speed =  SP.firstBallSpeed( target_ball_pos.dist( wm.ball().pos() ), dash_cycle + 1 );
         const Vector2D first_ball_vel = Vector2D::polar2vector( first_ball_speed, player_to_goal_angle.degree() );
         const Vector2D kick_accel = first_ball_vel - wm.ball().vel();
         const double kick_power = kick_accel.r() / wm.self().kickRate();
 
         dlog.addText( Logger::TEAM,
                       __FILE__": dist = %.2f, first_ball_speed = %.2f, (vx, vy) = (%.2f, %.2f)",
-                      ( ball_pos - wm.self().pos() ).r(), first_ball_speed,
+                      ( target_ball_pos - wm.self().pos() ).r(), first_ball_speed,
                       first_ball_vel.x, first_ball_vel.y );
 
-
-        Vector2D test_ball_pos = wm.ball().pos();
-        Vector2D test_ball_vel = first_ball_vel;
-        dlog.addText( Logger::TEAM,
-                      __FILE__": [0]ball pos test: (x, y) = (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
-                      test_ball_pos.x, test_ball_pos.y,
-                      test_ball_vel.x, test_ball_vel.y );
-        test_ball_pos += test_ball_vel;
-        test_ball_vel *= SP.ballDecay();
-        dlog.addText( Logger::TEAM,
-                      __FILE__": [1]ball pos test: (x, y) = (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
-                      test_ball_pos.x, test_ball_pos.y,
-                      test_ball_vel.x, test_ball_vel.y );
-        test_ball_pos += test_ball_vel;
-        test_ball_vel *= SP.ballDecay();
-        dlog.addText( Logger::TEAM,
-                      __FILE__": [2]ball pos test: (x, y) = (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
-                      test_ball_pos.x, test_ball_pos.y,
-                      test_ball_vel.x, test_ball_vel.y );
-        test_ball_pos += test_ball_vel;
-        test_ball_vel *= SP.ballDecay();
-        dlog.addText( Logger::TEAM,
-                      __FILE__": [3]ball pos test: (x, y) = (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
-                      test_ball_pos.x, test_ball_pos.y,
-                      test_ball_vel.x, test_ball_vel.y );
+        debug_ball_move( wm.ball().pos(), first_ball_vel, dash_cycle );
 
         if ( agent->doKick( kick_power, player_to_goal_angle ) )
         {
@@ -98,7 +67,7 @@ Bhv_StraighDribble::execute( PlayerAgent * agent )
             return true;
         }
     }
-    agent->doDash(100, ( wm.ball().pos() - wm.self().pos() ).dir() );
+    agent->doDash( 100, ( wm.ball().pos() - wm.self().pos() ).dir() );
     return true;
 }
 
@@ -106,25 +75,26 @@ Bhv_StraighDribble::execute( PlayerAgent * agent )
 /*!
 
  */
-rcsc::Vector2D
-Bhv_StraighDribble::get_player_move_dist( const PlayerAgent * agent,
-                                          const int move_cycle )
+Vector2D
+Bhv_StraighDribble::get_player_move_pos( const PlayerAgent * agent,
+                                         const int move_cycle )
 {
-    const ServerParam & SP = ServerParam::i();
     const WorldModel & wm = agent->world();
+    const ServerParam & SP = ServerParam::i();
     const Vector2D their_goal_pos( SP.pitchLength() / 2.0, 0.0 );
 
-    Vector2D player_move_pos = wm.self().pos();
+    Vector2D after_player_pos = wm.self().pos();
     Vector2D player_vel = wm.self().vel();
 
     // 1st action(kick)
-    player_move_pos += player_vel;
+    after_player_pos += player_vel;
     dlog.addText( Logger::TEAM,
-                    __FILE__": [1]kick action: (x, y) (%.2f, %.2f) => (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
-                    wm.self().pos().x, wm.self().pos().y,
-                    player_move_pos.x, player_move_pos.y, player_vel.x, player_vel.y );
+                  __FILE__": [1]kick action: (x, y) (%.2f, %.2f) => (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
+                  wm.self().pos().x, wm.self().pos().y,
+                  after_player_pos.x, after_player_pos.y, player_vel.x, player_vel.y );
+    dlog.addCircle( Logger::TEAM, after_player_pos, wm.self().playerType().playerSize(), "black" );
 
-    AngleDeg player_to_goal_angle = ( their_goal_pos - player_move_pos ).dir() - wm.self().body();
+    AngleDeg player_to_goal_angle = ( their_goal_pos - after_player_pos ).dir() - wm.self().body();
     const double dash_rate = wm.self().dashRate() * SP.dashDirRate( player_to_goal_angle.degree() );
     Vector2D accel = Vector2D::polar2vector( SP.maxDashPower() * dash_rate, player_to_goal_angle );
 
@@ -132,14 +102,79 @@ Bhv_StraighDribble::get_player_move_dist( const PlayerAgent * agent,
     {
         // n cycle action(dash)
         player_vel = player_vel * wm.self().playerType().playerDecay() + accel;
-        player_move_pos += player_vel;
+        after_player_pos += player_vel;
         dlog.addText( Logger::TEAM,
                       __FILE__": [%d/%d]move action: dash_angle(%.1f) (x, y) = (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
                       cycle, move_cycle,
                       player_to_goal_angle.degree(),
-                      player_move_pos.x, player_move_pos.y, player_vel.x, player_vel.y );
+                      after_player_pos.x, after_player_pos.y, player_vel.x, player_vel.y );
+        dlog.addCircle( Logger::TEAM, after_player_pos, wm.self().playerType().playerSize(), "black" );
     }
-    dlog.addCircle( Logger::TEAM, player_move_pos, 0.05, "black" );
+    dlog.addCircle( Logger::TEAM, after_player_pos, wm.self().playerType().kickableMargin(), "yellow" );
+    dlog.addCircle( Logger::TEAM, after_player_pos, wm.self().playerType().kickableArea(), "red" );
 
-    return player_move_pos;
+    return after_player_pos;
+}
+
+/*-------------------------------------------------------------------*/
+/*!
+
+
+ */
+Vector2D
+Bhv_StraighDribble::get_player_to_ball_margin( const PlayerAgent * agent,
+                                               const Vector2D & after_player_pos,
+                                               const double kickable_area_rate )
+{
+    constexpr double must_margin = 0.1;
+    const WorldModel & wm = agent->world();
+    const ServerParam & SP = ServerParam::i();
+
+    const double base_margin = wm.self().playerType().playerSize() + SP.ballSize() + must_margin;
+    double margin = base_margin + ( wm.self().playerType().kickableArea() - must_margin - base_margin ) * kickable_area_rate;
+    margin = std::clamp( margin, base_margin, wm.self().playerType().kickableArea() - must_margin );
+
+    const Vector2D player_to_ball_margin = Vector2D::polar2vector( margin,
+                                                                   ( after_player_pos - wm.self().pos() ).dir() );
+    const Vector2D target_ball_pos = after_player_pos + player_to_ball_margin;
+    dlog.addText( Logger::TEAM,
+                  __FILE__": margin = %.2f = (%.2f - %.2f) * %.2f, (x, y) = (%.2f, %.2f) dir=%.2f",
+                  margin, wm.self().playerType().kickableArea(), base_margin, kickable_area_rate,
+                  player_to_ball_margin.x, player_to_ball_margin.y,
+                  target_ball_pos.x, target_ball_pos.y, ( after_player_pos - wm.self().pos() ).dir() );
+    dlog.addCircle( Logger::TEAM, target_ball_pos, SP.ballSize(), "red" );
+
+    return target_ball_pos;
+}
+
+/*-------------------------------------------------------------------*/
+/*!
+
+ */
+void
+Bhv_StraighDribble::debug_ball_move( const Vector2D & init_ball_pos,
+                                     const Vector2D & init_ball_vel,
+                                     const int dash_cycle )
+{
+    const ServerParam & SP = ServerParam::i();
+    Vector2D ball_pos = init_ball_pos;
+    Vector2D ball_vel = init_ball_vel;
+    const double ball_decay = ServerParam::i().ballDecay();
+    dlog.addText( Logger::TEAM,
+                  __FILE__": [0/%d]ball pos test: (x, y) = (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
+                  dash_cycle + 1,
+                  init_ball_pos.x, init_ball_pos.y,
+                  init_ball_vel.x, init_ball_vel.y );
+
+    for ( int cycle = 1; cycle <= dash_cycle + 1; ++cycle )
+    {
+        ball_pos += ball_vel;
+        ball_vel *= ball_decay;
+        dlog.addText( Logger::TEAM,
+                      __FILE__": [%d/%d]ball pos test: (x, y) = (%.2f, %.2f), (vx, vy) = (%.2f, %.2f)",
+                      cycle, dash_cycle + 1,
+                      ball_pos.x, ball_pos.y,
+                      ball_vel.x, ball_vel.y );
+        dlog.addCircle( Logger::TEAM, ball_pos, SP.ballSize(), "blue" );
+    }
 }
--- src/player/basic_actions/bhv_straight_dribble.h
+++ src/player/basic_actions/bhv_straight_dribble.h
@@ -24,8 +24,17 @@ public:
 
 private:
 
-    rcsc::Vector2D get_player_move_dist( const rcsc::PlayerAgent* agent,
-                                         const int dash_cycle );
+    rcsc::Vector2D get_player_move_pos( const rcsc::PlayerAgent* agent,
+                                        const int dash_cycle );
+
+    rcsc::Vector2D get_player_to_ball_margin( const rcsc::PlayerAgent * agent,
+                                              const rcsc::Vector2D & after_player_pos,
+                                              const double kickable_area_rate );
+
+    void debug_ball_move( const rcsc::Vector2D & ball_pos,
+                          const rcsc::Vector2D & ball_vel,
+                          const int dash_cycle );
+
 };
 
 #endif
